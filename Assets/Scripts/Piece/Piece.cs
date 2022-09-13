3a16ca1b0f2e1f9e9d698512c1ea5d6803ce4cfd
Ragnarok <s3rg10.martinez.roman@gmail.com>
2022-09-13 17:04:33 +0200

    First commit

---
+++ Assets/Scripts/Piece/Piece.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    [SerializeField] PieceType pieceType;
+    [SerializeField] SideType sideType;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Piece/PieceType.cs
@@ -0,0 +1,9 @@
+public enum PieceType 
+{
+    Pawn,
+    Bishop,
+    King,
+    Knight,
+    Queen,
+    Rook
+}
