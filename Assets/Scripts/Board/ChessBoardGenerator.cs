cacccbe895107f3ca439b4cab581db5f9ba41c2e
Ragnarok <s3rg10.martinez.roman@gmail.com>
2022-09-14 20:18:29 +0200

    - Fix center to generate all tiles - Yoffset

--- Assets/Scripts/Board/ChessBoardGenerator.cs
+++ Assets/Scripts/Board/ChessBoardGenerator.cs
@@ -8,16 +8,20 @@ public class ChessBoardGenerator : MonoBehaviour
 {
     [Header("Art stuff")]
     [SerializeField] private Material tileMaterial;
+    [SerializeField] private float tileSize = 1.0f;
+    [SerializeField] private float yOffset = 0.2f;
+    [SerializeField] private Vector3 boardCenter = Vector3.zero;
 
     private const int TILE_COUNT_X = 8;
     private const int TILE_COUNT_Y = 8;
     private GameObject[,] tiles;
     private Camera currentCamera;
     private Vector2Int currentHover;
+    private Vector3 bounds;
 
     void Awake()
     {
-        GenerateAllTiles(1, TILE_COUNT_X, TILE_COUNT_Y);
+        GenerateAllTiles(tileSize, TILE_COUNT_X, TILE_COUNT_Y);
     }
 
     private void Update() 
@@ -27,9 +31,15 @@ public class ChessBoardGenerator : MonoBehaviour
             currentCamera = Camera.main;
             return;
         }
+        HighlightTile();
+        
+    }
 
+    private void HighlightTile()
+    {
         RaycastHit info;
         Ray ray = currentCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
         if (Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile", "Hover")))
         {
             // Get the infexes of the tile i've hit
@@ -61,14 +71,17 @@ public class ChessBoardGenerator : MonoBehaviour
     }
 
     // Generate the board
-    private void GenerateAllTiles(int tileSize, int tileCountX, int tileCountY)
+    private void GenerateAllTiles(float tileSize, int tileCountX, int tileCountY)
     {
+        yOffset += transform.position.y;
+        bounds = new Vector3((tileCountX / 2) * tileSize, 0, (tileCountX / 2) * tileSize) + boardCenter;
+
         tiles = new GameObject[tileCountX, tileCountY];
         for (int x = 0; x < tileCountX; x++)
             for (int y = 0; y < tileCountY; y++)
                 tiles[x,y] = GenerateSingleTile(tileSize, x, y);
     }
-    private GameObject GenerateSingleTile(int tileSize, int x, int y)
+    private GameObject GenerateSingleTile(float tileSize, int x, int y)
     {
         GameObject tileObject = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
         tileObject.transform.parent = transform;
@@ -78,20 +91,21 @@ public class ChessBoardGenerator : MonoBehaviour
         tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
 
         Vector3[] vertices = new Vector3[4];
-        vertices[0] = new Vector3(x * tileSize, 0, y * tileSize);
-        vertices[1] = new Vector3(x * tileSize, 0, (y+1) * tileSize);
-        vertices[2] = new Vector3((x+1) * tileSize, 0, y * tileSize);
-        vertices[3] = new Vector3((x+1) * tileSize, 0, (y+1) * tileSize);
+        vertices[0] = new Vector3(x * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[1] = new Vector3(x * tileSize, yOffset, (y+1) * tileSize) - bounds;
+        vertices[2] = new Vector3((x+1) * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[3] = new Vector3((x+1) * tileSize, yOffset, (y+1) * tileSize) - bounds;
 
         int[] tris = new int[] { 0, 1, 2, 1, 3, 2 };
 
         mesh.vertices = vertices;
         mesh.triangles = tris;
 
-        mesh.RecalculateNormals();
-
         tileObject.layer = LayerMask.NameToLayer("Tile");
-        tileObject.AddComponent<BoxCollider>();
+        tileObject.AddComponent<BoxCollider>().size = new Vector3(tileSize, 0.1f, tileSize);
+
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
 
         return tileObject;
     }
